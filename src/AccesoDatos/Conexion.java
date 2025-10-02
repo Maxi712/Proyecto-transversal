7c6bb1899750438ad2c0ef2471131e2a3c0ae7d5
Pablo Gimenez <pablitosanluis2002@gmail.com>
2025-10-02 18:43:09 -0300

    Update Conexion.java

--- src/AccesoDatos/Conexion.java
+++ src/AccesoDatos/Conexion.java
@@ -26,6 +26,31 @@ public class Conexion {
             try{
                 Class.forName("org.mariadb.jdbc.Driver");
                 conexion = DriverManager.getConnection(url+db, usuario, pass);
+            //Actualizar un alumno
+            String sql = "UPDATE alumno SET nombre = 'Rodrigo' WHERE dni = ???";
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            int registros = ps.executeUpdate();
+            System.out.println(registros);
+
+            //Eliminar un alumno
+            String sql = "DELETE FROM alumno WHERE dni = ???";
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            int registros = ps.executeUpdate();
+            System.out.println(registros);
+
+            //Obtener todos los alumnos
+            String sql = "SELECT * FROM alumno";
+            //WHERE apellido like ('e%')";
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ResultSet resultado = ps.executeQuery();
+            while(resultado.next()){
+                System.out.println("ID: " +resultado.getInt("idAlumno"));
+                System.out.println("DNI: " +resultado.getInt("dni"));
+                System.out.println("Apellido: " +resultado.getString("apellido"));
+                System.out.println("Nombre: " +resultado.getString("nombre"));
+                System.out.println("Fecha de nacimiento: " +resultado.getDate("fechaNacimiento")
+                System.out.println("Estado: " +resultado.getBoolean("estado"));
+            }
             }catch(ClassNotFoundException ex){
                 JOptionPane.showMessageDialog(null, "Error al cargar los drivers ..."+ex.getMessage());
             }catch(SQLException ex){
